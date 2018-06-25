54cfd87f6f2b787a4b3e153c5acd2cc62ce0eb45
kobi1023 <kobi1023@gmail.com>
2018-06-25 11:12:39 +0300

    Update WaistCircumferenceScreen.js

--- screens/WaistCircumferenceScreen.js
+++ screens/WaistCircumferenceScreen.js
@@ -1,1 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Slider, Button } from 'react-native-elements';
+
+export default class WaistCircumferenceScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            waist: 95,
+        }
+    }
+    static navigationOptions = {
+        title: "",
+        header: null
+    };
+    render() {
+        var {navigate} = this.props.navigation;
+        return (
+        <View style={styles.container}>
+            <Text>Waist Circumference: {this.state.waist}</Text>
+            <Slider
+                style = {{width: 300}}
+                value = {this.state.waist}
+                step = {1}
+                minimumValue = {50}
+                maximumValue = {210}
+                onValueChange = {(value) => this.setState({waist: value})} 
+            />
+            <Button
+              onPress = {() => navigate("StatusAndGoals", {})}
+              title="GO TO STATUS & GOALS"
+              titleStyle={{fontWeight: 'bold', fontSize: 18}}
+              containerStyle={{marginVertical: 10, height: 40, width: 200}}
+            />
+        </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
