83afbcd412ffa9d498bf94f8de6279b7e0671459
kobi1023 <kobi1023@gmail.com>
2018-06-25 11:15:26 +0300

    Adding new screens to Navigation

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import IntroScreen from './screens/IntroScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MeasurementsScreen from './screens/MeasurementsScreen';
 import StatusAndGoalsScreen from './screens/StatusAndGoalsScreen';
+import HeightScreen from './screens/HeightScreen';
+import WaistCircumferenceScreen from './screens/WaistCircumferenceScreen';
+import WeightScreen from './screens/WeightScreen';
 
 const Navigation = createStackNavigator({
     Intro : {screen: IntroScreen},
     Welcome : {screen: WelcomeScreen},
     Measurements : {screen: MeasurementsScreen},
     StatusAndGoals : {screen: StatusAndGoalsScreen},
+    Height : {screen: HeightScreen},
+    WaistCircumference : {screen: WaistCircumferenceScreen},
+    Weight : {screen: WeightScreen},
 });
 export default Navigation;
