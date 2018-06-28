f60932d619bb19a484039d6904b54905fb856e6c
kobi1023 <kobi1023@gmail.com>
2018-06-28 16:16:47 +0300

    Adding basic weight status

--- screens/StatusAndGoalsScreen.js
+++ screens/StatusAndGoalsScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import ABSI from '../calculators/absi';
+import BMI from '../calculators/bmi';
+import BMR from '../calculators/bmr';
 
 export default class StatusAndGoalsScreen extends React.Component {
     static navigationOptions = {
@@ -10,17 +12,33 @@ export default class StatusAndGoalsScreen extends React.Component {
     };
 
     componentDidMount() {
-        const absiScore = ABSI.calculateABSIScore(24.7, 191, 94, 42, "male");
-        console.log("absi Score: " + absiScore);
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('/users/' + userId).once('value').then((snapshot) => {
+            const weight = snapshot.val().measurements.weight;
+            const height = napshot.val().measurements.height;
+            const age = napshot.val().age;
+            const gender = napshot.val().gender
+            const waist = napshot.val().measurements.waistWC;
+            
+            const bmi = BMI.calculateBMI(.weight, height);
+            const bmiDescription = BMI.description(bmi);
+            const absiz = ABSI.calculateABSIScore(bmi, height, waist, age, gender);
+            const absizDescription = ABSI.description(absiz);
+
+            this.setState({bmiDescription: bmiDescription});
+            this.setState({absizDescription: absizDescription});
+    //     this.setState({email: snapshot.val().email});
+    //     this.setState({gender: snapshot.val().gender});
+    //     this.setState({birthday: snapshot.val().birthday});
+        });
     }
 
     render() {
         return (
         <View style={styles.container}>
             <Text>Status & Goals:</Text>
-            <Text>Weight Status: Normal Weight</Text>
-            <Text>Ideal Weight: 83.6</Text>
-            <Text>First Goal Weight: 89</Text>
+            <Text>bmiDescription: {this.state.bmiDescription}</Text>
+            <Text>absizDescription: {this.state.absizDescription}</Text>
             <Avatar
                   size="xlarge"
                   rounded
