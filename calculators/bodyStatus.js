d9f620615c5dea74aa1d49114a5833c62dfd9783
kobi1023 <kobi1023@gmail.com>
2018-07-09 17:03:57 +0300

    Update bodyStatus.js

--- calculators/bodyStatus.js
+++ calculators/bodyStatus.js
@@ -1,25 +1,10 @@
 import ABSI from '.absi';
 import BMI from '.bmi';
-import IdealWeight from '.idealWeight';
-
-var items = [
-  [1, 2],
-  [3, 4],
-  [5, 6]
-];
 
 var BodyStatus = {
     calculateStatus: function(weight, height, waistCircum, age,  gender) {
-        const bmi = BMI.calculateBMI(weight, height);
-        const absiz = ABSI.calculateABSIScore(bmi, height, waist, age, gender);
-
-        const idealWeightDevine = IdealWeight.calculateDevineFormula(height, age, gender);
-        const idealWeightHamwi = IdealWeight.calculateHamwiFormula(height, age, gender);
-        const idealWeightMiller = IdealWeight.calculateMillerFormula(height, age, gender);
-            
-        // Over Weight
-        // Skinny
-        // Skinny Fat
+        const bmi = new BMI(weight, height);
+        const absiz = new ABSI(bmi, height, waist, age, gender);
        
         let status = 0;
         if (isOverWeight(bmi, absiz))
@@ -43,28 +28,28 @@ var BodyStatus = {
 
 function isOverWeight(bmi, absiz) {  
    
-   // if (bmi >= overweight & absiz >= high)
+   // if (bmi.status >= bmi.statusEnum.overweight & absiz.status >= absiz.statusEnum.high)
     
    return true;
 }
 
 function isSkinnyFat(bmi, absiz) {  
    
-   // if (bmi = Underweight & absiz >= high) 
+   // if (bmi.status = bmi.statusEnum.underweight & absiz.status >= absiz.statusEnum.high) 
     
    return true;
 }
 
 function isSkinny(bmi, absiz) {
     
-   // if (bmi = Underweight & absiz = very low) 
+   // if (bmi.status = bmi.statusEnum.underweight & absiz.status = absiz.statusEnum.veryLow) 
     
    return true;
 }
 
 function isNormalWeight(bmi, absiz) {
     
-   // if (bmi <= normal weight & absiz = average) 
+   // if (bmi.status <= bmi.statusEnum.normalWeight & absiz.status = absiz.statusEnum.average) 
     
    return true;
 }
