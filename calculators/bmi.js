2f7fa8e92d2262eec43734c320c92c62eb649bb8
kobi1023 <kobi1023@gmail.com>
2018-07-09 17:52:49 +0300

    Convert BMI into an Object

--- calculators/bmi.js
+++ calculators/bmi.js
@@ -1,24 +1,34 @@
-var BMI = {
-    calculateBMI: function(weight, height) {
-        const formatedHeight = (height / 100).toFixed(2);
-        const bmi = (weight / Math.pow(formatedHeight,2)).toFixed(2);
-        return bmi;
-    },
+export const StatusEnum = {"underweight":1, "normalWeight":2, "overweight":3, "obesity": 4};
 
-    description: function(bmi){
-        let description = "";
-
-        if (bmi < 18.5)
-            description = "Underweight";
-        else if (bmi >= 18.5 && bmi <= 24.9)
-            description = "Normal Weight";
-        else if (bmi >= 25 && bmi <= 29)
-            description = "Overweight";
-        else if (bmi >= 30)
-            description = "Obesity";
-
-        return description;
+function BMI (weight, height) {
+    // Calculate BMI.
+    const formatedHeight = (height / 100).toFixed(2);
+    const bmi = (weight / Math.pow(formatedHeight,2)).toFixed(2);
+    this.bmi;
+    
+    // Set Status & Description.
+    if (bmi < 18.5) {
+        this.status = statusEnum.underweight;
+        this.description = "Underweight";
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        this.status = statusEnum.normalWeight;
+        this.description = "Normal Weight";
+    }
+    else if (bmi >= 25 && bmi <= 29) {
+        this.status = statusEnum.overweight;
+        this.description = "Overweight";
+    }
+    else if (bmi >= 30) {
+        this.status = statusEnum.obesity;
+        this.description = "Obesity";
     }
 }
 
-module.exports = BMI;
+BMI.prototype.status = function () {
+	return this.status;
+}
+
+BMI.prototype.description = function () {
+	return this.description
+}
