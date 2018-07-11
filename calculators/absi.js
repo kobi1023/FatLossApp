615245d26d39b352dd581e045e01946e977bdc19
kobi1023 <kobi1023@gmail.com>
2018-07-11 16:47:17 +0300

    Convert ABSI to calls object

--- calculators/absi.js
+++ calculators/absi.js
@@ -1,31 +1,47 @@
-var ABSI = {
-    calculateABSIScore: function(bmi, height, waistCircum, age,  gender) {
+export const ABSIstatusEnum = {"veryLow":1, "low":2, "average":3, "high": 4, "veryHigh": 5};
+
+export default class ABSI {
+    constructor(bmi, height, waistCircum, age,  gender){
         const formatedWaist = waistCircum / 100;
         const formatedHeight = height / 100;
         
-        const absi = formatedWaist / (Math.pow(bmi, 2/3) * Math.pow(formatedHeight, 1/2));    
+    	// Calculate ABSI.
+        this.absi = formatedWaist / (Math.pow(bmi, 2/3) * Math.pow(formatedHeight, 1/2)); 
+	
+	// Calculate ABSIz.
         const absiFactors = getAbsiMeanAndStandardDeviation(gender, age);
-        
-        const absiz = (absi - absiFactors.ABSIMean) / absiFactors.ABSISD;
-        
-        return absiz.toFixed(5);
-    },
-    
-    description: function(absiz){
-        let description = "";
-
-        if (absiz < -0.868)
-            description = "Very Low";
-        else if (absiz >= -0.868 && absiz <= -0.272)
+        this.absiz = (absi - absiFactors.ABSIMean) / absiFactors.ABSISD;
+        this.absiz.toFixed(5);
+	    
+	// Set Status & Description.
+    	if (absiz < -0.868){
+	    this.status = ABSIstatusEnum.veryLow;	
+            this.description = "Very Low";
+	}
+        else if (absiz >= -0.868 && absiz <= -0.272){
+	    this.status = ABSIstatusEnum.low;	
             description = "Low";
-        else if (absiz >= -0.272 && absiz <= 0.229)
+	}
+        else if (absiz >= -0.272 && absiz <= 0.229){
+	    this.status = ABSIstatusEnum.average;	
             description = "Average";
-	else if (absiz >= 0.229 && absiz <= 0.798)
-            description = "High";    
-        else if (absiz > 0.798)
+	}
+	else if (absiz >= 0.229 && absiz <= 0.798){
+	    this.status = ABSIstatusEnum.high;	
+            description = "High";
+	}
+        else if (absiz > 0.798){
+	    this.status = ABSIstatusEnum.veryHigh;	
             description = "Very High";
+	}
+    }
+    
+    status(){
+        return this.status;
+    }
 
-        return description;
+    description(){
+        return this.description
     }
 }
 
@@ -41,5 +57,3 @@ function getAbsiMeanAndStandardDeviation(gender, age) {
     
 	return {ABSIMean: val.ABSIMean , ABSISD: val.ABSISD};
 }
-
-module.exports = ABSI;
