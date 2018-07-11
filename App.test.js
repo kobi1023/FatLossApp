c96ab6653082adc6f2642f1142614ab1d763305e
kobi1023 <kobi1023@gmail.com>
2018-07-11 16:30:31 +0300

    Added BMI tests

--- App.test.js
+++ App.test.js
@@ -12,7 +12,14 @@ import {expect} from 'chai';
 // });
 
 //BMI
-it('should be normalWeight for: height=191 & weight=90', () => {
-  const bmi = new BMI(191, 90);
-  expect(bmi.status).to.equal(BMIstatusEnum.normalWeight);
-});
+describe('BMI Tests', function () {
+  it('should be normalWeight for: height=191 & weight=90', () => {
+    const bmi = new BMI(191, 90);
+    expect(bmi.status).to.equal(BMIstatusEnum.normalWeight);
+  });
+  
+  it('should be bmi value 24.7 for: height=191 & weight=90', () => {
+    const bmi = new BMI(191, 90);
+    expect(bmi.status).to.equal(BMIstatusEnum.normalWeight);
+  });
+}
