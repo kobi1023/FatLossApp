bce2264abe40a418f41984026558a5020dbb462b
kobi1023 <kobi1023@gmail.com>
2018-07-11 16:54:57 +0300

    Added ABSI tests

--- App.test.js
+++ App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import App from './App';
 import BMI from './calculators/bmi';
 import {BMIstatusEnum} from './calculators/bmi';
+import ABSI from './calculators/absi';
+import {ABSIstatusEnum} from './calculators/absi';
 import {expect} from 'chai';
 
 // import renderer from 'react-test-renderer';
@@ -11,15 +13,19 @@ import {expect} from 'chai';
 //   expect(rendered).toBeTruthy();
 // });
 
-//BMI
+//BMI.
 describe('BMI Tests', function () {
   it('should be normalWeight for: height=191 & weight=90', () => {
     const bmi = new BMI(191, 90);
     expect(bmi.status).to.equal(BMIstatusEnum.normalWeight);
   });
-  
-  it('should be bmi value 24.7 for: height=191 & weight=90', () => {
-    const bmi = new BMI(191, 90);
-    expect(bmi.status).to.equal(BMIstatusEnum.normalWeight);
-  });
 }
+         
+//ABSI.
+describe('ABSI Tests', function () {
+  it('should be low for : height=191, weight=90, age=42, gender=male, waist=93', () => {
+    const bmi = new BMI(191, 90);
+    const absi = new ABSI(bmi.status, 191, 93, 42,  "male");
+    expect(absi.status).to.equal(ABSIstatusEnum.low);
+  });
+}      
