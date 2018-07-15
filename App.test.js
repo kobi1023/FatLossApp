111ec887a059acdcfcaf8abb3698961a3915e942
kobi1023 <kobi1023@gmail.com>
2018-07-15 15:25:07 +0300

    Added -fit- Test

--- App.test.js
+++ App.test.js
@@ -53,6 +53,12 @@ describe('Body Shape Tests:', function () {
     expect(bodyShape.status).to.equal(BodyShapeStatusEnum.fit);
   });
   
+  it('should be -fit- for: gender=female, age=22, height=155, weight=52, waist=64', () => {
+    const userProfile = {"gender": "female", "age": 22, "height": 155, "weight": 52, "waist": 64}
+    const bodyShape = new BodyShape(userProfile);
+    expect(bodyShape.status).to.equal(BodyShapeStatusEnum.fit);
+  });
+  
   it('should be -athletic- for: gender=female, age=40, height=174, weight=67, waist=67', () => {
     const userProfile = {"gender": "female", "age": 40, "height": 174, "weight": 67, "waist": 74}
     const bodyShape = new BodyShape(userProfile);
