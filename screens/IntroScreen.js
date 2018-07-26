ea8fb1c07bcdac287628e375092e19a6a5e649aa
kobi1023 <kobi1023@gmail.com>
2018-07-26 14:44:39 +0300

    Added Onboarding screen

--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, AlertIOS } from 'react-native';
+import { StyleSheet, Text, View, AlertIOS, Image  } from 'react-native';
 import { Button } from 'react-native-elements';
 import firebase from '../plugins/firebase';
+import Onboarding from 'react-native-onboarding-swiper';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -20,7 +21,39 @@ export default class App extends React.Component {
       <View style={styles.container}>
         {/* <Text>By tapping Log In, you agree to our</Text> */}
         {/* <Text>Terms and Privacy Policy.</Text> */}
-        <Button
+        <Onboarding
+          pages={[
+            {
+              backgroundColor: '#fff',
+              image: <Image source={require('./images/circle.png')} />,
+              title: 'Onboarding',
+              subtitle: 'Done with React Native Onboarding Swiper',
+            },
+            {
+              backgroundColor: '#fe6e58',
+              image: <Image source={require('./images/square.png')} />,
+              title: 'The Title',
+              subtitle: 'This is the subtitle that sumplements the title.',
+            },
+            {
+              backgroundColor: '#fff',
+              image: <Image source={require('./images/triangle.png')} />,
+              title: 'GET STARTED',
+              subtitle: (
+                <Button
+                  onPress = {
+                    () => logIn(navigate)
+                  }
+                  title="LOG IN WITH FACEBOOK"
+                  titleStyle={{fontWeight: 'bold', fontSize: 18}}
+                  buttonStyle={{backgroundColor: "rgba(59,89,152,1)", borderWidth: 0, borderColor: 'transparent', borderRadius: 20}}
+                  containerStyle={{marginVertical: 10, height: 40, width: 200}}
+                />
+              ),
+            },
+          ]}
+        />
+        {/* <Button
               onPress = {
                 () => logIn(navigate)
               }
@@ -28,7 +61,7 @@ export default class App extends React.Component {
               titleStyle={{fontWeight: 'bold', fontSize: 18}}
               buttonStyle={{backgroundColor: "rgba(59,89,152,1)", borderWidth: 0, borderColor: 'transparent', borderRadius: 20}}
               containerStyle={{marginVertical: 10, height: 40, width: 200}}
-        />
+        /> */}
         {/* <Text>We dont post anything to Facebook.</Text> */}
       </View>
     );
@@ -68,8 +101,8 @@ async function logIn(navigate) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
